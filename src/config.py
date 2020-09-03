9e64a4c2f22e572882de52a706075d2ae239f5be
Junsen Li <33750446+JohnsonLee98@users.noreply.github.com>
2020-09-04 00:06:25 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -74,6 +74,8 @@ class Config(object):
         # self.train_dir = ['../../CoNSeP/Train/%s/'  % data_code_dict[self.model_type]]
         # self.valid_dir = ['../../CoNSeP/Valid/%s/' % data_code_dict[self.model_type]]
         self.train_dir = ['../../CoNSeP/Train/Images']
+        self.valid_dir = ['../../CoNSeP/Valid/Images']
+
         # self.valid_dir = ['../../CoNSeP//Image' % data_code_dict[self.model_type]]
         # number of processes for parallel processing input
         self.nr_procs_train = 8 

745c6cf31b6cc69fe171cbf214222e8a1bc8bfb2
Junsen Li <33750446+JohnsonLee98@users.noreply.github.com>
2020-09-12 13:21:57 +0800

    Update graph.py

--- src/model/graph.py
+++ src/model/graph.py
@@ -117,7 +117,6 @@ class Model(ModelDesc, Config):
 
     # def _get_inputs(self):
     def inputs(self):
-        print(self.train_input_shape)
         return [tf.TensorSpec( [None, self.train_input_shape[0],self.train_input_shape[1], 3],tf.float32, 'images'),
                 tf.TensorSpec( [None, self.train_mask_shape[0],self.train_mask_shape[1],None], tf.float32,'truemap-coded')]
     
@@ -137,7 +136,7 @@ class Model(ModelDesc, Config):
 ####
 class Model_NP_HV(Model):
     # def _build_graph(self, inputs):
-    def build_graph(self, *inputs):
+    def _build_graph(self, *inputs):
         
         # images, truemap_coded = inputs
         images = inputs[0] 
